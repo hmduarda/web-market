2fa45938266e35fdf85aa37024ada5c93bfeb57c
João Roberto <joaorobertogalvao.2008@gmail.com>
2026-03-11 13:10:08 -0300

    feat: criando o productService e implementando o CRUD

--- server/src/models/Product.ts
+++ server/src/models/Product.ts
@@ -1,39 +1,44 @@
-const mongoose = require("mongoose");
+import mongoose, { Document, Schema } from "mongoose";
 
-const productSchema = new mongoose.Schema(
-  {
-    nome: {
-      type: String,
-      required: true,
+export interface IProduct extends Document {
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  image: string;
+  createdAt: Date;
+}
+
+const productSchema = new Schema<IProduct>(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        stock: {
+            type: Number,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
     },
-    precoAtual: {
-      type: Number,
-      required: true,
-      min: 0,
-    },
-    precoPromocao: {
-      type: Number,
-      default: null,
-      min: 0,
-    },
-    tipo: {
-      type: String,
-      required: true,
-    },
-    descricao: {
-      type: String,
-      required: true,
-    },
-    dataValidade: {
-      type: Date,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
+
 );
 
-const Product = mongoose.model("Product", productSchema);
 
-module.exports = Product;
+const Product = mongoose.model<IProduct>("Product", productSchema);
+
+export default Product;
